ed9a222e51fb2d925165ea436ca4e0b273746ec9
Amber Alter <aalter@univera.com>
2014-12-02 14:45:08 -0800

    Add components and styles from different pages to front-page template

--- wp-content/themes/serve-first/front-page.php
+++ wp-content/themes/serve-first/front-page.php
@@ -11,46 +11,125 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
                     <section id="call-to-action">
-                        <div class="indent">
-                            <div class="front-left">
-                                <h2 class="section-title">Call to Action</h2>
-                            </div>
-                            <div class="front-right">
-                                Form
-                            </div>
-                        </div>
-                    </section>
+                        <div class="indent clear">
+                                        <?php 
+                                        $query = new WP_Query( 'pagename=book-an-appointment' );
+                                        // The Loop
+                                        if ( $query->have_posts() ) {
+                                                while ( $query->have_posts() ) {
+                                                        $query->the_post();
+                                                        echo '<div class="entry-content">';
+                                                        the_content();
+                                                        echo '</div>';
+                                                }
+                                        }
+
+                                        /* Restore original Post Data */
+                                        wp_reset_postdata();
+                                        ?>
+                        </div><!-- .indent -->
+                </section><!-- #call-to-action -->
+                
                     <section id="testimonials">
                         <div class="indent">
                         Testimonials
                         </div>
                     </section>
-                    <section id="services">
-                        <div class="indent">
-                            <h2 class="section-title">Services</h2>
-                            some text goes here
-                            <ul>
-                                <li>Item One</li>
-                                <li>Item Two</li>
-                                <li>Item Three</li>
-                            </ul>
-                        </div>
-                    </section>
-                    <section id="meet">
-                        <div class="indent">
-                            <h2 class="section-title">Meet the Doctor</h2>
-                        </div>
-                    </section>
-                    <section id="call-to-action">
-                        <div class="indent">
-                            <div class="front-left">
-                                <h2 class="section-title">Contact us</h2>
-                            </div>
-                            <div class="front-right">
-                             Address
-                            </div>
-                        </div>
-                    </section>
+                
+                
+                   
+                <section id="services">
+                        <div class="indent clear">
+                                <?php 
+                                $query = new WP_Query( 'pagename=services' );
+                                $services_id = $query->queried_object->ID;
+
+                                // The Loop
+                                if ( $query->have_posts() ) {
+                                        while ( $query->have_posts() ) {
+                                                $query->the_post();
+                                                echo '<h2 class="section-title">' . get_the_title() . '</h2>';
+                                                echo '<div class="entry-content">';
+                                                the_content('');
+                                                echo '</div>';
+                                        }
+                                }
+
+                                /* Restore original Post Data */
+                                wp_reset_postdata();
+
+                                $args = array(
+                                        'post_type' => 'page',
+                                        'post_parent' => $services_id
+                                );
+                                $services_query = new WP_Query( $args );
+
+                                // The Loop
+                                if ( $services_query->have_posts() ) {
+
+                                        echo '<ul class="services-list">';
+                                        while ( $services_query->have_posts() ) {
+                                                $services_query->the_post();
+                                                echo '<li class="clear">';
+                                                echo '<a href="' . get_permalink() . '" title="Learn more about ' . get_the_title() . '">';
+                                                echo '<h3 class="services-title">' . get_the_title() . '</h3>';
+                                                echo '</a>';
+                                                echo '<div class="services-lede">';
+                                                the_content();
+                                                echo '</div>';
+                                                echo '</li>';
+                                        }
+                                        echo '</ul>';
+                                }
+
+                                /* Restore original Post Data */
+                                wp_reset_postdata();
+                                ?>
+                        </div><!-- .indent -->
+                </section><!--#services-->
+			
+                <section id="meet">
+                        <div class="indent clear">
+                                <?php 
+                                $query = new WP_Query( 'pagename=meet-dr-britton' );
+                                // The Loop
+                                if ( $query->have_posts() ) {
+                                        while ( $query->have_posts() ) {
+                                                $query->the_post();
+                                                echo '<h2 class="section-title">' . get_the_title() . '</h2>';
+                                                echo '<div class="entry-content">';
+                                                the_content();
+                                                echo '</div>';
+                                        }
+                                }
+
+                                /* Restore original Post Data */
+                                wp_reset_postdata();
+                                ?>
+                        </div><!-- .indent -->
+                </section><!-- #meet -->
+
+                <section id="contact">
+                        <div class="indent clear">
+                                <?php 
+                                $query = new WP_Query( 'pagename=contact' );
+                                // The Loop
+                                if ( $query->have_posts() ) {
+                                        while ( $query->have_posts() ) {
+                                                $query->the_post();
+                                                echo '<h2 class="section-title">' . get_the_title() . '</h2>';
+                                                echo '<div class="entry-content">';
+                                                the_content();
+                                                echo '</div>';
+                                        }
+                                }
+
+                                /* Restore original Post Data */
+                                wp_reset_postdata();
+                                ?>
+                        </div><!-- .indent -->
+                </section><!-- #contact -->
+                
 
 			
 
