6aa39ab06ccb9bc8192cac424ef568e1a7463821
Amber Alter <aalter@univera.com>
2014-12-09 14:50:12 -0800

    Went back to old main nav colors, added pages to front page and adjusted colors. Changed footer

--- wp-content/themes/serve-first/footer.php
+++ wp-content/themes/serve-first/footer.php
@@ -15,7 +15,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'serve-first' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'serve-first' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'serve-first' ), 'serve-first', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'serve-first' ), 'Serve First', '<a href="http://amberalter.com" rel="designer">Amber</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- wp-content/themes/serve-first/front-page.php
+++ wp-content/themes/serve-first/front-page.php
@@ -34,14 +34,14 @@ global $more; //allows truncation of page content displayed on single page view
                         </div><!-- .indent -->
                 </section><!-- #call-to-action -->
                 
-                   <section id="testimonials">
+                 <section id="testimonials">
                         <div class="indent clear">
                                 <?php 
                                 $args = array(
-                                        'posts_per_page' => 4,
+                                        'posts_per_page' => 2,
                                         'orderby' => 'ID',
                                         'order' =>'ASC',
-                                        'category_name' => 'get-involved'
+                                        'category_name' => 'statistics'
                                 );
 
                                 $query = new WP_Query( $args );
@@ -72,64 +72,16 @@ global $more; //allows truncation of page content displayed on single page view
                         </div><!-- .indent -->
                 </section><!-- #testimonials -->
                 
+                  
+                
                 
                    
-                <section id="services">
-                        <div class="indent clear">
-                                <?php 
-                                $query = new WP_Query( 'pagename=services' );
-                                $services_id = $query->queried_object->ID;
-
-                                // The Loop
-                                if ( $query->have_posts() ) {
-                                        while ( $query->have_posts() ) {
-                                                $query->the_post();
-                                                $more = 0; //sets more variable to false
-                                                echo '<h2 class="section-title">' . get_the_title() . '</h2>';
-                                                echo '<div class="entry-content">';
-                                                the_content('');
-                                                echo '</div>';
-                                        }
-                                }
-
-                                /* Restore original Post Data */
-                                wp_reset_postdata();
-
-                                $args = array(
-                                        'post_type' => 'page',
-                                        'post_parent' => $services_id
-                                );
-                                $services_query = new WP_Query( $args );
-
-                                // The Loop
-                                if ( $services_query->have_posts() ) {
-
-                                        echo '<ul class="services-list">';
-                                        while ( $services_query->have_posts() ) {
-                                                $services_query->the_post();
-                                                $more = 0; //sets more variable to false
-                                                echo '<li class="clear">';
-                                                echo '<a href="' . get_permalink() . '" title="Learn more about ' . get_the_title() . '">';
-                                                echo '<h3 class="services-title">' . get_the_title() . '</h3>';
-                                                echo '</a>';
-                                                echo '<div class="services-lede">';
-                                                the_content();
-                                                echo '</div>';
-                                                echo '</li>';
-                                        }
-                                        echo '</ul>';
-                                }
-
-                                /* Restore original Post Data */
-                                wp_reset_postdata();
-                                ?>
-                        </div><!-- .indent -->
-                </section><!--#services-->
+                
 			
                 <section id="meet">
                         <div class="indent clear">
                                 <?php 
-                                $query = new WP_Query( 'pagename=meet-dr-britton' );
+                                $query = new WP_Query( 'pagename=map' );
                                 // The Loop
                                 if ( $query->have_posts() ) {
                                         while ( $query->have_posts() ) {
